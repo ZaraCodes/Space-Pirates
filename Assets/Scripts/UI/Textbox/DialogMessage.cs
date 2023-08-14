027e1298942deb9b6f2705c72e3b20a866514662
ZaraCodes <wejink441@gmail.com>
2023-08-14 11:50:59 +0200

    MYAAA

--- Assets/Scripts/UI/Textbox/DialogMessage.cs
+++ Assets/Scripts/UI/Textbox/DialogMessage.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.ShaderGraph;
 using UnityEngine;
-using UnityEngine.Localization;
 using UnityEngine.UI;
 
 public class DialogMessage : MonoBehaviour
