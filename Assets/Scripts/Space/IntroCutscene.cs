cdd139845b5ceebe2e3d91c1a676745b2e95a4e2
ZaraCodes <wejink441@gmail.com>
2023-08-16 16:13:08 +0200

    More Intro Cutscene UwU

--- Assets/Scripts/Space/IntroCutscene.cs
+++ Assets/Scripts/Space/IntroCutscene.cs
@@ -20,38 +20,84 @@ public class IntroCutscene : MonoBehaviour
 
     /// <summary>Reference to the audio source that plays the theme for captain meow</summary>
     [SerializeField] private AudioSource captainMeowMusic;
+
+    /// <summary>Transform where the ship will be moved to in the cutscene</summary>
+    [SerializeField] private Transform spawn;
+
+    /// <summary>Reference to Captain Meow's ship</summary>
+    [SerializeField] private GameObject meowShip;
+
+    /// <summary>Tracks if captain meow has appeared in the cutscene, which would trigger a different dialog when hitting the trigger</summary>
+    private bool captainMeowAppeared;
+
+    /// <summary>Reference to the second dialog</summary>
+    [SerializeField] private TextboxTrigger captainMeowAppearsDialog;
+
+    /// <summary>Reference to the cat teleport animation object</summary>
+    [SerializeField] private GameObject catTeleportAnimation;
+
+    /// <summary>Reference to the dialog with Captain Meow</summary>
+    [SerializeField] private TextboxTrigger captainMeowDialog;
+
+    /// <summary>Reference to the dialog after the cat got teleport</summary>
+    [SerializeField] private TextboxTrigger postTeleportDialog;
     #endregion
 
     #region Methods
     /// <summary>Starts the ship movement before the dialog</summary>
     private void StartShipMovement()
     {
+        shipRigidbody.bodyType = RigidbodyType2D.Kinematic;
         shipRigidbody.velocity = startingVelocity;
+    }
+    
+    /// <summary>Stops the ship movement</summary>
+    private void StopShipMovement()
+    {
+        shipRigidbody.bodyType = RigidbodyType2D.Static;
     }
 
     /// <summary>Starts the intro cutscene</summary>
-    private void BeginCutscene()
+    private void BeginPreMeowDialog()
     {
         UnityEvent onFadeInFinished = new();
         onFadeInFinished.AddListener(() =>
         {
             preMeowDialog.LoadDialog();
-            shipRigidbody.bodyType = RigidbodyType2D.Static;
+            StopShipMovement();
         });
         StartCoroutine(GameManager.Instance.PauseMenuHandler.FadeIn(onFadeInFinished));
     }
 
-
+    /// <summary>
+    /// Starts the second part of the intro cutscene where captain meow appears
+    /// </summary>
     public void StartMeowSection()
     {
-        StartCoroutine(CrossFadeMusic());
+        captainMeowAppeared = true;
+
+        meowShip.SetActive(true);
+        shipRigidbody.gameObject.transform.position = spawn.position;
+        StartShipMovement();
+
+        UnityEvent onFadeOutFinished = new();
+        onFadeOutFinished.AddListener(() => {
+            captainMeowAppearsDialog.LoadDialog();
+            GameManager.Instance.PauseMenuHandler.FadeTime = 3f;
+            StartCoroutine(CrossFadeMusic());
+        });
+
+        GameManager.Instance.PauseMenuHandler.FadeTime = 2f;
+        StartCoroutine(GameManager.Instance.PauseMenuHandler.FadeOut(onFadeOutFinished));
     }
     
+    /// <summary>Coroutine that fades the music</summary>
+    /// <returns></returns>
     private IEnumerator CrossFadeMusic()
     {
         captainMeowMusic.Play();
         var limit = .4f;
-        var timer = 4f;
+        var timer = 5f;
         var maxTime = timer;
 
         while (timer > 0)
@@ -64,19 +110,51 @@ public class IntroCutscene : MonoBehaviour
         yield return null;
         preMeowMusic.Stop();
     }
+
+    /// <summary>Starts the third dialog</summary>
+    private void BeginCaptainMeowDialog()
+    {
+        UnityEvent onFadeInFinished = new();
+        onFadeInFinished.AddListener(() =>
+        {
+            StopShipMovement();
+            captainMeowDialog.LoadDialog();
+        });
+        StartCoroutine(GameManager.Instance.PauseMenuHandler.FadeIn(onFadeInFinished));
+    }
+
+    public void ShowCatTeleportAnimation()
+    {
+        catTeleportAnimation.SetActive(true);
+        StartCoroutine(DelayPostTeleportDialog());
+    }
+
+    private IEnumerator DelayPostTeleportDialog()
+    {
+        yield return new WaitForSeconds(3.5f);
+        postTeleportDialog.LoadDialog();
+        catTeleportAnimation.SetActive(false);
+    }
     #endregion
 
     #region Unity Stuff
     private void Start()
     {
         GameManager.Instance.PauseMenuHandler.FadeTime = 3f;
-        StartShipMovement();
     }
 
+    private void Update()
+    {
+        if (!captainMeowAppeared && GameManager.Instance.IsFading)
+        {
+            StartShipMovement();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        BeginCutscene();
+        if (captainMeowAppeared) BeginCaptainMeowDialog();
+        else BeginPreMeowDialog();
     }
     #endregion
 }
