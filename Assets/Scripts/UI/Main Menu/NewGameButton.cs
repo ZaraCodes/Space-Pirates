33939fbacaed8016fd1e9e147a15793b5084f578
ZaraCodes <wejink441@gmail.com>
2024-03-03 23:18:27 +0100

    Start Button now has a sound :3

--- Assets/Scripts/UI/Main Menu/NewGameButton.cs
+++ Assets/Scripts/UI/Main Menu/NewGameButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -13,7 +14,12 @@ public class NewGameButton : MonoBehaviour
     [SerializeField] private GameObject sceneChooser;
     /// <summary>Reference to the main menu</summary>
     [SerializeField] private GameObject mainMenu;
-    
+
+    [SerializeField] private AudioSource buttonAudio;
+
+    [SerializeField] private Image fadeImage;
+
+    public UnityEvent OnFadeFinished;
     /// <summary>
     /// Opens the scene chooser window
     /// </summary>
@@ -29,6 +35,31 @@ public class NewGameButton : MonoBehaviour
     public void StartNewGame()
     {
         SceneManager.LoadScene(5);
+    }
+
+    public void StartButtonPressed(float fadeTime)
+    {
+        buttonAudio.Play();
+        StartFadeIn(fadeTime);
+    }
+
+    public void StartFadeIn(float fadeTime)
+    {
+        StartCoroutine(FadeOut(fadeTime));
+    }
+
+    private IEnumerator FadeOut(float fadeTime)
+    {
+        fadeImage.gameObject.SetActive(true);
+        float timer = 0;
+        while (timer < fadeTime)
+        {
+            timer += Time.deltaTime;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, timer / fadeTime);
+            Debug.Log(timer/fadeTime);
+            yield return null;
+        }
+        OnFadeFinished.Invoke();
     }
 
     /// <summary>
